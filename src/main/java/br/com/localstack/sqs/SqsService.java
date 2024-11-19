842abf8fb368f431f92cb04e416144f894b49ec9
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-19 00:33:03 -0300

    feat: buscar mensagem sqs

--- src/main/java/br/com/localstack/sqs/SqsService.java
+++ src/main/java/br/com/localstack/sqs/SqsService.java
@@ -1,10 +1,14 @@
 package br.com.localstack.sqs;
 
 import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SqsService {
@@ -20,5 +24,12 @@ public class SqsService {
                 .withMessageBody(messageBody)
                 .withMessageGroupId(messageGroupId);
         amazonSQS.sendMessage(sendMessageRequest);
+    }
+
+    public List<Message> receiveMessages() {
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl)
+                .withWaitTimeSeconds(5)
+                .withMaxNumberOfMessages(10);
+        return amazonSQS.receiveMessage(receiveMessageRequest).getMessages();
     }
 }
--- src/main/java/br/com/localstack/sqs/controllers/SqsController.java
+++ src/main/java/br/com/localstack/sqs/controllers/SqsController.java
@@ -1,10 +1,13 @@
 package br.com.localstack.sqs.controllers;
 
 import br.com.localstack.sqs.SqsService;
+import com.amazonaws.services.sqs.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SqsController {
@@ -16,6 +19,11 @@ public class SqsController {
     public String sendMessage(@RequestParam String messageBody, @RequestParam String messageGroupId) {
         sqsService.sendMenssage(messageBody, messageGroupId);
         return "Mensagem enviada com sucesso!";
+    }
+
+    @GetMapping("/receive-messages")
+    public List<Message> receiveMessages() {
+        return sqsService.receiveMessages();
     }
 
 }
